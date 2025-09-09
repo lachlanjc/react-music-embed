0a273458232c102597802f03a593c159fd9ba5ca
Lachlan Campbell <lachlanjc@hey.com>
2025-09-08 21:42:04 -0700

    Migrate to functional component

--- src/index.js
+++ src/index.js
@@ -1,39 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-export default class Embed extends Component {
-  static propTypes = {
-    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-    url: PropTypes.string.isRequired
-  }
+export default function Embed({ url, width, height, style, ...props }) {
+  const src = url.replace('//music.apple.com', '//embed.music.apple.com')
 
-  static defaultProps = {
-    width: 512,
-    height: 150,
-    url: 'https://music.apple.com/us/album/ultralife/1440894720?i=1222147721'
-  }
+  return (
+    <iframe
+      {...props}
+      allow="autoplay *; encrypted-media *;"
+      frameBorder="0"
+      height={height}
+      style={{
+        width: '100%',
+        maxWidth: width,
+        overflow: 'hidden',
+        background: 'transparent',
+        ...style,
+      }}
+      sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+      src={src}
+    />
+  )
+}
 
-  render() {
-    const { url, width, height, ...props } = this.props
-    const src = url.replace('//music.apple.com', '//embed.music.apple.com')
+Embed.defaultProps = {
+  width: 512,
+  height: 150,
+  url: 'https://music.apple.com/us/album/ultralife/1440894720?i=1222147721',
+}
 
-    return (
-      <iframe
-        {...props}
-        allow="autoplay *; encrypted-media *;"
-        frameBorder="0"
-        height={height}
-        style={{
-          width: '100%',
-          maxWidth: width,
-          overflow: 'hidden',
-          background: 'transparent',
-          ...props.style
-        }}
-        sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
-        src={src}
-      />
-    )
-  }
+Embed.propTypes = {
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  url: PropTypes.string.isRequired,
 }
